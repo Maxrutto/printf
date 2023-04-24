218fb5e5952d000f1fa2823b3c5abc672ba51835
Maxrutto <ruttomaxmillan@gmail.com>
2023-04-24 01:20:07 -0700

    main.h

--- main.h
+++ main.h
@@ -2,3 +2,8 @@
  * _printf - A function that produces output according to a format
  */
 int _printf(const char *format, ...);
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+int _putchar(char c);
